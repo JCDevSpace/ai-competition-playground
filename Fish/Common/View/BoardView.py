f8b2099318f59bce0e98ac737f32318f0d31fb18
jerrygitsis <gitsis.a@husky.neu.edu>
2020-10-05 21:58:28 -0400

    view for fish game is renderable

--- Fish/Common/View/BoardView.py
+++ Fish/Common/View/BoardView.py
@@ -1,6 +1,34 @@
-from Tkinter import *
+from tkinter import *
+import math
+from TileView import TileView
 
 class BoardView:
+    BG_COLOR = "white"
 
-    def __init__(self, board):
-        
+    def __init__(self, initial_board_state):
+        self.frame = Tk()
+
+        width = len(initial_board_state[0])
+        height = len(initial_board_state)
+
+        self.canvas = Canvas(self.frame, bg=self.BG_COLOR, width=self.calculate_frame_width(width), height=self.calculate_frame_height(height))
+
+        self.draw_game_state(initial_board_state)
+
+    def calculate_frame_height(self, height):
+        return (height * TileView.TILE_SIZE) + TileView.TILE_SIZE
+
+    def calculate_frame_width(self, width):
+        return (width * TileView.TILE_SIZE * 4) + TileView.TILE_SIZE
+
+    def draw_game_state(self, game_state):
+        for row in range(0, len(game_state)):
+            for col in range(0, len(game_state[row])):
+                tile = TileView(row, col, game_state[row][col])
+                tile.draw(self.canvas)
+
+        self.canvas.pack()
+
+if __name__ == '__main__':
+    BoardView([[1, "red", "brown"], [1, 2, 3], [4, -1, 5]])
+    mainloop()
