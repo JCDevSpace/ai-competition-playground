07b676c4c5679d86de7f4c5b4ed2bccab5f4e783
matthewlarsen <larsen.ma@husky.neu.edu>
2020-10-07 20:48:35 -0400

    Forgot to push some changes to board and referee

--- Fish/Common/Controller/Referee.py
+++ Fish/Common/Controller/Referee.py
@@ -2,16 +2,45 @@ import random
 
 class Referee:
 
-    def __init__(self, rows=None, cols=None):
+    def __init__(self, rows=None, cols=None, uniform=False, uniform_fish_num = None, min_holes=0, min_one_fish=0, specific_holes = []):
+
         if not rows:
             rows = random.randint(2, 7)
         if not cols:
             cols = random.randint(2, 7)
 
-        self.board = Board(rows, cols)
-        self.generate_board()
+        if rows < 1 or cols < 1:
+            raise IllegalArgumentException("Rows and Cols must be a positive number")
+        if (uniform and min_holes) or (uniform and min_one_fish):
+            raise IllegalArgumentException("min_holes and min_one_fish areguments are not allowed when uniform is True")
+        if uniform and not uniform_fish_num:
+            raise IllegalArgumentException("A uniform board needs the uniform_fish_num specified in range [1, 5]")
 
-        self.board_view = BoardView(self.board)
+        # Same number of fish everywhere, no holes
+        self.board = None
+
+        if uniform and type(uniform_fish_num) == int and uniform_fish_num <= 5 and uniform_fish_num >= 1:
+            self.generate_uniform_board(rows, cols, uniform_fish_num)
+        elif min_holes or min_one_fish:
+            self.generate_non_uniform_board(rows, cols, min_holes, min_one_fish)
+        elif specific_holes:
+            pass
+        else:
+            rand_holes = random.randint(0, (rows + cols)//4)
+            rand_one_fish = random.randint(0, (rows + cols)//4)
+            self.generate_non_uniform_board(rows, cols, rand_holes, rand_one_fish)
+
+        self.board_view = BoardView(self.board.get_board_state())
 
 
-    def generate_board():
+    def generate_uniform_board(self, rows, cols, fish_num):
+        if rows < 1 or cols < 1:
+            raise IllegalArgumentException("Cannot generate a uniform board with non-positive rows and columns")
+        if fish_num < 1 or fish_num > 5:
+            raise IllegalArgumentException("Uniform board must have between [1 and 5] fish")
+        self.board = Board(rows, cols, num_fish=fish_num)
+
+    def generate_non_uniform_board(self, rows, cols, min_holes, min_one_fish):
+        board = Board(rows, cols, min_ones=min_one_fish)
+        while board.hole_count() < min_holes:
+            board.add_hole(random.randint(0, rows-1), random.randint(0, cols-1))
--- Fish/Common/Model/Board.py
+++ Fish/Common/Model/Board.py
@@ -18,21 +18,47 @@ class Board:
 #   Tile is one of (Int, Color)
 #       Color is one of "red", "white", "brown", "black"
 
-    def __init__(self, rows, cols, num_fish=None):
+    def __init__(self, rows, cols, num_fish=None, min_ones = 0):
         layout = []
+        ones = 0
         for y in range(rows):
             layout.append([])
             for x in range(cols):
                 if num_fish:
                     layout[y].append(num_fish)
                 else:
-                    layout[y].append(random.randint(1, 5))
+                    layout[y].append(random.randint(1,5))
 
         self.layout = layout
+        self.rows = rows
+        self.cols = cols
+        self.min_ones = min_ones
+
+        self.odd_row_moves = [(2, 0), (-2, 0), (-1, 0), (-1, 1), (1, 0), (1, 1)]
+        self.even_row_moves= [(2, 0), (-2, 0), (-1, -1), (-1, 0), (1, -1), (1, 0)]
+
+        self.assert_enough_ones()
+
 
     ## Pregame methods
+
+    def assert_enough_ones(self, min_ones=self.min_ones):
+        ones = 0
+        for y in range(self.rows):
+            for x in range(self.cols):
+                if self.layout[y][x] == 1:
+                    ones += 1
+
+        while ones < self.min_ones:
+            randrow = random.randint(0, rows-1)
+            randcol = random.randint(0, cols-1)
+            if self.layout[randrow][randcol] != 1:
+                self.layout[randrow][randcol] = 1
+                ones += 1
+
     def add_hole(self, row, col):
         self.layout[row][col] = -1
+        self.assert_enough_ones()
 
     def set_fish(self, num_fish, row, col):
         self.layout[row][col] = num_fish
@@ -43,6 +69,15 @@ class Board:
         self.layout[row][col] = player.get_color()
         return score
 
+    def hole_count(self):
+        holes = 0
+        for y in range(self.rows):
+            for x in range(self.cols):
+                if self.layout[y][x] == -1:
+                    holes += 1
+        return holes
+
+        
     # Play methods
 
     def remove_penguin(self, player, row, col):
@@ -52,7 +87,7 @@ class Board:
             raise IllegalArgumentException("Cannot remove another player's penguin")
 
     def move(self, player, old_row, old_col, new_row, new_col):
-        if (new_row, new_col) in self.get_valid_moves(player, old_row, old_col, new_row, new_col):
+        if (new_row, new_col) in self.get_valid_moves(player, old_row, old_col):
             self.remove_penguin(player, old_row, old_col)
             return self.place_penguin(player,new_row, new_col)
         else:
@@ -63,7 +98,32 @@ class Board:
         if self.layout[row][col] != player.get_color():
             return []
         else:
-            pass
+            valid_moves = []
+            for dir_index in range(len(self.odd_row_moves)):
+                valid_moves += self.valid_in_dir(row, col, dir_index)
+            return valid_moves
+
+    def is_valid_square(self, row, col):
+        row_valid = row >= 0 and row <  self.rows
+        col_valid = col >= 0 and col < self.cols
+        square_valid = type(self.layout[row][col]) == int and self.layout[row][col] >= 1
+        return row_valid and col_valid and square_valid
+
+
+    def valid_in_dir(self, row, col, dir_index):
+        if row % 2 == 1:
+            moves = self.odd_row_moves
+        else:
+            moves = self.even_row_moves
+        valid_moves = []
+        (delta_row, delta_col) = moves[dir_index]
+        new_pos = (row + delta_row, col + delta_col)
+
+        if self.is_valid_square(new_pos[0], new_pos[1]):
+            valid_moves.append(new_pos)
+            valid_moves += self.valid_in_dir(new_pos[0], new_pos[1], dir_index)
+
+        return valid_moves
 
     def get_board_state(self):
         return self.layout
