fab0df303ec5de3978de7c97ff16715d1551256f
jerrygitsis <gitsis.a@husky.neu.edu>
2020-10-15 16:54:22 -0400

    artist tests

---
+++ Fish/Common/UnitTests/FishViewTest.py
@@ -0,0 +1,31 @@
+import unittest
+
+from Common.View.FishView import FishView
+from Common.View.FishView import DEFAULT_STYLE
+
+threebythree = FishView(([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [], {}, 0))
+fourbytwo = FishView(([[1, 1], [1, 1], [-1, 0], [3, 4]], [], {}, 0))
+twobyfive = FishView(([[1, -1, 0, 5, 2], [4, 2, 0, -1, -1]], [], {}, 0))
+
+class TestFishViewHeight(unittest.TestCase):
+    def test_frame_height_3x3(self):
+        self.assertEqual(4 * DEFAULT_STYLE['tile_size'], threebythree.calculate_frame_height())
+
+    def test_frame_height_4x2(self):
+        self.assertEqual(5 * DEFAULT_STYLE['tile_size'], fourbytwo.calculate_frame_height())
+
+    def test_frame_height_2x5(self):
+        self.assertEqual(3 * DEFAULT_STYLE['tile_size'], twobyfive.calculate_frame_height())
+
+class TestFishViewWidth(unittest.TestCase):
+    def test_frame_width_3x3(self):
+        self.assertEqual(13 * DEFAULT_STYLE['tile_size'], threebythree.calculate_frame_width())
+
+    def test_frame_width_4x2(self):
+        self.assertEqual(9 * DEFAULT_STYLE['tile_size'], fourbytwo.calculate_frame_width())
+
+    def test_frame_width_2x5(self):
+        self.assertEqual(21 * DEFAULT_STYLE['tile_size'], twobyfive.calculate_frame_width())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Fish/Common/UnitTests/TileArtistTest.py
@@ -0,0 +1,24 @@
+import unittest
+
+from Common.View.TileArtist import TileArtist
+from Common.View.FishView import DEFAULT_STYLE
+
+class TestTileArtistOutline(unittest.TestCase):
+    def test_tile_outline(self):
+        tile1 = TileArtist(DEFAULT_STYLE['tile_size'] * 8,
+                           DEFAULT_STYLE['tile_size'] * 2,
+                           4,
+                           DEFAULT_STYLE)
+
+        outline = [DEFAULT_STYLE['tile_size'] * 9, DEFAULT_STYLE['tile_size'] * 2,
+                   DEFAULT_STYLE['tile_size'] * 10, DEFAULT_STYLE['tile_size'] * 2,
+                   DEFAULT_STYLE['tile_size'] * 11, DEFAULT_STYLE['tile_size'] * 3,
+                   DEFAULT_STYLE['tile_size'] * 10, DEFAULT_STYLE['tile_size'] * 4,
+                   DEFAULT_STYLE['tile_size'] * 9, DEFAULT_STYLE['tile_size'] * 4,
+                   DEFAULT_STYLE['tile_size'] * 8, DEFAULT_STYLE['tile_size'] * 3]
+
+        self.assertEqual(outline, tile1.get_tile_outline())
+
+
+if __name__ == '__main__':
+    unittest.main()
