809de515b3e7d63c0b5b937778b2e92da5c5132d
Jing Chen <jcdevspace@gmail.com>
2021-02-12 15:59:12 -0500

    lifted up state to GameView component

--- src/view/src/Game.js
+++ src/view/src/Game.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Component } from "react";
 import StatusPanel from './StatusPanel';
 import CanvasBoard from './CanvasBoard';
 
@@ -15,64 +14,30 @@ const GameType = styled.h3`
   background-color: lightcoral;
 `;
 
-const GameState = styled.div`
+const State = styled.div`
   display: flex;
   width: 80em;
   justify-content: space-around;
 `;
 
-class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      players: [
-        "azure", "crimson"
-      ],
-      scores: {
-        "crimson": 20,
-        "azure": 10
-      },
-      gameType: "fish",
-      layout: [
-        [0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0],
-        [0,1,0,1,0,1,0,1],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [1,0,1,0,1,0,1,0],
-        [0,1,0,1,0,1,0,1],
-        [1,0,1,0,1,0,1,0]
-      ],
-      avatars: {
-        "crimson": [[5, 0], [5, 2], [5, 4], [5, 6], 
-          [6, 1], [6, 3], [6, 5], [6, 7], 
-          [7, 0], [7, 2], [7, 4], [7, 6]],
-        "azure": [[0, 0],[0, 1], [0, 3], [0, 5], [0, 7], 
-          [1, 0], [1, 2], [1, 4], [1, 6], 
-          [2, 1], [2, 3], [2, 5], [2, 7]],
-      }  
-    }
-  }
-
-  render() {
-    return (
-      <GameInfo>
-        <GameType>{this.state.gameType}</GameType>
-        <GameState>
-          <CanvasBoard 
-            tileSize={100} 
-            gameType={this.state.gameType} 
-            layout={this.state.layout} 
-            avatars={this.state.avatars} 
-          />
-          <StatusPanel 
-            players={this.state.players} 
-            scores={this.state.scores}
-          />
-        </GameState>
-      </GameInfo>
-    );
-  }
+const Game = (props) => {
+  return (
+    <GameInfo>
+      <GameType>{props.gameState.gameType}</GameType>
+      <State>
+        <CanvasBoard 
+          tileSize={100} 
+          gameType={props.gameState.gameType} 
+          layout={props.gameState.layout} 
+          avatars={props.gameState.avatars} 
+        />
+        <StatusPanel 
+          players={props.gameState.players} 
+          scores={props.gameState.scores}
+        />
+      </State>
+    </GameInfo>
+  );
 }
 
 export default Game;
--- src/view/src/GameView.js
+++ src/view/src/GameView.js
@@ -16,14 +16,42 @@ class GameView extends Component {
         [["Jing", "Max"], ["Steve", "Bess"], ["Bob", "Dug"]],
         [["Jing", "Steve"], ["Joel", "Dug"]],
         [["Jing", "Joel"]]
-      ]
+      ],
+      currentGame: {
+        players: [
+          "azure", "crimson"
+        ],
+        scores: {
+          "crimson": 20,
+          "azure": 10
+        },
+        gameType: "checker",
+        layout: [
+          [0,1,0,1,0,1,0,1],
+          [1,0,1,0,1,0,1,0],
+          [0,1,0,1,0,1,0,1],
+          [0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0],
+          [1,0,1,0,1,0,1,0],
+          [0,1,0,1,0,1,0,1],
+          [1,0,1,0,1,0,1,0]
+        ],
+        avatars: {
+          "crimson": [[5, 0], [5, 2], [5, 4], [5, 6], 
+            [6, 1], [6, 3], [6, 5], [6, 7], 
+            [7, 0], [7, 2], [7, 4], [7, 6]],
+          "azure": [[0, 0],[0, 1], [0, 3], [0, 5], [0, 7], 
+            [1, 0], [1, 2], [1, 4], [1, 6], 
+            [2, 1], [2, 3], [2, 5], [2, 7]],
+        }
+      }
     }
   }
 
   render() {
     return (
       <ViewContainer>
-        <Game />
+        <Game gameState={this.state.currentGame} />
         <Tournament roundHistory={this.state.roundHistory} />
       </ViewContainer>
     );
